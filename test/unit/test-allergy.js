d179f404ee0ab62aaf6c0d78ec11592e43967100
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2014-10-24 23:09:58 -0400

    allergy unit tests

---
+++ test/unit/test-allergy.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var chai = require('chai');
+
+var bbm = require('../../index.js');
+
+var samples = require('../samples/unit/allergy');
+
+var expect = chai.expect;
+
+var codeToPath = function (errors) {
+    return errors.reduce(function (result, error) {
+        var paths = result[error.code];
+        if (!paths) {
+            result[error.code] = paths = [];
+        }
+        paths.push(error.path);
+        paths.sort();
+        return result;
+    }, {});
+};
+
+describe('allergy unit tests', function () {
+    it('sample valid_0', function () {
+        bbm.validator.validateSectionObj(samples.valid_0, 'allergy');
+        var result = bbm.validator.getLastError();
+        expect(result.valid).to.be.true;
+    });
+
+    it('sample valid_1', function () {
+        bbm.validator.validateSectionObj(samples.valid_1, 'allergy');
+        var result = bbm.validator.getLastError();
+        expect(result.valid).to.be.true;
+    });
+
+    it('sample invalid_0', function () {
+        bbm.validator.validateSectionObj(samples.invalid_0, 'allergy');
+        var result = bbm.validator.getLastError();
+        expect(result.valid).to.be.false;
+        expect(result.errors).to.have.length(2);
+        var c2p = codeToPath(result.errors);
+        expect(c2p.ARRAY_LENGTH_SHORT).to.have.length(1);
+        expect(c2p.ARRAY_LENGTH_SHORT[0]).to.equal("#/identifiers");
+        expect(c2p.INVALID_TYPE).to.have.length(1);
+        expect(c2p.INVALID_TYPE[0]).to.equal("#/observation/reactions");
+    });
+
+    it('sample invalid_1', function () {
+        bbm.validator.validateSectionObj(samples.invalid_1, 'allergy');
+        var result = bbm.validator.getLastError();
+        expect(result.valid).to.be.false;
+        expect(result.errors).to.have.length(2);
+        var c2p = codeToPath(result.errors);
+        expect(c2p.OBJECT_ADDITIONAL_PROPERTIES).to.have.length(1);
+        expect(c2p.OBJECT_ADDITIONAL_PROPERTIES[0]).to.equal("#/observation");
+        expect(c2p.OBJECT_PROPERTIES_MINIMUM).to.have.length(1);
+        expect(c2p.OBJECT_PROPERTIES_MINIMUM[0]).to.equal("#/observation/allergen");
+    });
+});
